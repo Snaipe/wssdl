9feda0fbc937f73b852a09d25e4a4984f690c8db
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2016-11-05 09:26:05 +0100

    test: Added reverse parsing tests

---
+++ test/tests/suffix.lua
@@ -0,0 +1,39 @@
+--
+--  Copyright 2016 diacritic <https://diacritic.io>
+--
+--  This file is part of wssdl <https://github.com/diacritic/wssdl>.
+--
+--  wssdl is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  wssdl is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with wssdl.  If not, see <http://www.gnu.org/licenses/>.
+
+local wssdl = require 'wssdl'
+
+wstest.autosuite 'suffix' {
+
+  basic = {
+
+    pkt = wssdl.packet {
+      payload : bytes();
+      suffix : u32();
+    };
+
+    actual = 'aaaaaaaaaaaaaaaa' .. wstest.pack('>I4', 3149642683);
+
+    expected = {
+      payload = ByteArray.new('aaaaaaaaaaaaaaaa'),
+      suffix  = 3149642683
+    }
+
+  }
+
+}
